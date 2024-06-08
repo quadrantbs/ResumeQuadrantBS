d48ab94169ae1e5cc3a4ce4e2db70f3602c2d91d
Quadrant Barokatus Salam <quadrantbs@gmail.com>
2024-06-08 19:22:43 +0700

    add js collapsible

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// Get the elements that will be used to trigger the appearance changes
-const toggleButtons = document.querySelectorAll('.toggle-button');
-const sections = document.querySelectorAll('.cv-section');
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-// Add click event listeners to each toggle button
-toggleButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Toggle the active class on the button to change its appearance
-    button.classList.toggle('active');
-
-    // Toggle the hidden class on the corresponding section to show or hide it
-    const sectionId = button.dataset.section;
-    const section = document.querySelector(`#${sectionId}`);
-    section.classList.toggle('hidden');
+for (i = 0; i < coll.length; i++){
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight= null;
+    } else {
+      content.style.maxHeight= content.scrollHeight + "px";
+    }
   });
-});
+}
