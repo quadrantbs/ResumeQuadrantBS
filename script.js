ccf1614a6f6f7e3443c40625e415a1258056d2fd
Quadrant Barokatus Salam <quadrantbs@gmail.com>
2024-06-10 14:01:32 +0700

    rounded collapsible and loading circle

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ for (i = 0; i < coll.length; i++){
     }
   });
 }
+
+let loading = document.getElementById("loading");
+
+setTimeout(function(){
+  loading.style.opacity = 0;
+  setTimeout(function(){
+    loading.style.display= "none";
+  }, 750);
+}, 250);
